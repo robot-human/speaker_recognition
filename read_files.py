625740a214bc60ab8e3a0ccaaa933b83146bc018
prodriguez <rodriguez_p@fev.com>
2022-11-04 09:09:29 -0600

    commit message

--- read_files.py
+++ read_files.py
@@ -13,4 +13,4 @@ for speaker in database_dir:
     #print("*****************************")
     #print(" ")
 
-print(speaker_sessions['911'][0])
+print(database_path+"/911/"+speaker_sessions['911'][0])

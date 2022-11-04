4297e84213e5fe22a87d788dded72c73860c563b
prodriguez <rodriguez_p@fev.com>
2022-11-04 08:32:32 -0600

    commit message

--- read_files.py
+++ read_files.py
@@ -1,6 +1,6 @@
 import os
 
-database_path = "/Data"
+database_path = "/home/ar/Data/ivette_thesis/data_bases/Librispeech/train-clean-100/train-clean-100/"
 path = "/Users/humanrobot/Documents/data/simpsons"
 
 dir = os.listdir(database_path)

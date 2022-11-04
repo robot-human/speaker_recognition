520182590fdfc6c9572867144c97361f13d661e7
prodriguez <rodriguez_p@fev.com>
2022-11-04 08:41:31 -0600

    commit message

--- read_files.py
+++ read_files.py
@@ -1,9 +1,10 @@
 import os
 
-database_path = "/media/Data/data_bases/Librispeech/train-clean-100/train-clean-100/"
-path = "/Users/humanrobot/Documents/data/simpsons"
+print(os.getcwd())
+#database_path = "/media/Data/data_bases/Librispeech/train-clean-100/train-clean-100/"
+#path = "/Users/humanrobot/Documents/data/simpsons"
 
-dir = os.listdir(database_path)
+#dir = os.listdir(database_path)
 
-for file in dir:
-    print(file)
+#for file in dir:
+#    print(file)

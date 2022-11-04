1c586f9796696911b5332eb8c53a1969b424c4fa
prodriguez <rodriguez_p@fev.com>
2022-11-04 09:33:04 -0600

    commit message

--- read_files.py
+++ read_files.py
@@ -7,14 +7,16 @@ speaker_files = {}
 for speaker in database_dir:
     speaker_path = database_path + "/" + speaker
     sessions_dir = os.listdir(speaker_path)
+    speaker_files_list = []
     for session in sessions_dir:
         audio_files_path = speaker_path + "/" + session
         audio_files_dir = os.listdir(audio_files_path)
         for audio_file in audio_files_dir:
             if(audio_file.find(".flac") > 0):
-                print(audio_files_path+"/"+audio_file)
+                speaker_files_list.append(audio_files_path+"/"+audio_file)
+    speaker_files[speaker] = speaker_files_list
 
-
+print(speaker_files)
     #speaker_sessions[speaker] = files_dir
     #print(files_dir)
     #print("*****************************")

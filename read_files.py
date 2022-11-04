a22fb2d89a7672ff2cb7f4f41e779ad7059bafdb
prodriguez <rodriguez_p@fev.com>
2022-11-04 09:16:56 -0600

    commit message

--- read_files.py
+++ read_files.py
@@ -3,11 +3,12 @@ import os
 database_path = "/media/Data/databases/LibriSpeech/train-clean-100/train-clean-100"
 database_dir = os.listdir(database_path)
 
-speaker_sessions = {}
+speaker_files = {}
 for speaker in database_dir:
     path = database_path + "/" + speaker
-    files_dir = os.listdir(path)
-    print(files_dir)
+    sessions_dir = os.listdir(path)
+    for session in sessions_dir:
+        print(session)
     #speaker_sessions[speaker] = files_dir
     #print(files_dir)
     #print("*****************************")

2bbaedec5746f047e66d942dd236cd99431a13d7
prodriguez <rodriguez_p@fev.com>
2022-11-04 08:26:52 -0600

    commit message

--- read_files.py
+++ read_files.py
@@ -1,6 +1,6 @@
 import os
 
-database_path = "~/Data/databases/LibriSpeech/train-clean-100/train-clean-100"
+database_path = "~/"
 path = "/Users/humanrobot/Documents/data/simpsons"
 
 dir = os.listdir(database_path)

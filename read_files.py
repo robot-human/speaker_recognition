9d535c379c41d6a230fda728ee7de91d1ee97821
prodriguez <rodriguez_p@fev.com>
2022-11-04 09:22:39 -0600

    commit message

--- read_files.py
+++ read_files.py
@@ -11,7 +11,9 @@ for speaker in database_dir:
         audio_files_path = speaker_path + "/" + session
         audio_files_dir = os.listdir(audio_files_path)
         for audio_file in audio_files_dir:
-            print(audio_files_dir)
+            print(audio_file)
+
+
     #speaker_sessions[speaker] = files_dir
     #print(files_dir)
     #print("*****************************")

ddd175a9dc36238484051a90c1a2743fd98ac504
prodriguez <rodriguez_p@fev.com>
2022-11-04 11:53:33 -0600

    commit message

--- copy_files.sh
+++ copy_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash  
 # Start Bash script
 
-scp ar@ardl2:/media/Data/pedro_tesis/log_files/fig1.png /Users/humanrobot/Documents/tesis/speaker_recognition/log_files
+scp ar@ardl2:/media/Data/pedro_tesis/log_files/fig1.png /Users/humanrobot/Documents/tesis/speaker_recognition/log_files/

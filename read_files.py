7db0819f12156e613e68c05e9fefe90cb9d4ab39
prodriguez <rodriguez_p@fev.com>
2022-11-04 08:18:49 -0600

    commit 1

--- read_files.py
+++ read_files.py
@@ -3,7 +3,7 @@ import os
 database_path = "/Data/databases/LibriSpeech/train-clean-100/train-clean-100/"
 path = "/Users/humanrobot/Documents/data/simpsons"
 
-dir = os.listdir(path)
+dir = os.listdir(database_path)
 
 for file in dir:
-    print(database_path)
+    print(file)

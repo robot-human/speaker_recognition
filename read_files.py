eaa6889a4fac1f153f2a279711ee184e61c3f48c
prodriguez <rodriguez_p@fev.com>
2022-11-04 09:15:53 -0600

    commit message

--- read_files.py
+++ read_files.py
@@ -5,15 +5,15 @@ database_dir = os.listdir(database_path)
 
 speaker_sessions = {}
 for speaker in database_dir:
-    #print(speaker)
     path = database_path + "/" + speaker
     files_dir = os.listdir(path)
-    speaker_sessions[speaker] = files_dir
+    print(files_dir)
+    #speaker_sessions[speaker] = files_dir
     #print(files_dir)
     #print("*****************************")
     #print(" ")
 
-new_path = database_path+"/911/"+speaker_sessions['911'][0]
-new_path_dir = os.listdir(new_path)
-for files in new_path_dir:
-    print(files)
+#new_path = database_path+"/911/"+speaker_sessions['911'][0]
+#new_path_dir = os.listdir(new_path)
+#for files in new_path_dir:
+#    print(files)

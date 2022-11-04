d577e6d7db504d281bcb1b18e573e35152326a69
prodriguez <rodriguez_p@fev.com>
2022-11-04 12:33:10 -0600

    commit message

--- read_files.py
+++ read_files.py
@@ -23,13 +23,16 @@ for speaker in database_dir:
     speaker_ids.append(speaker)
     speaker_files[speaker] = speaker_files_list
 
-samples, sample_rate = librosa.load(speaker_files[speaker_ids[0]][0], mono=True, sr=8000)
 
 
+for path in speaker_files[speaker_ids[0]]:
+    print(path)
 
-functions.samples_to_seconds(len(samples),sample_rate)
-new_samples = feats.vad(samples, 0.01)
-functions.samples_to_seconds(len(new_samples),sample_rate)
-final_samples = functions.trim_signal(new_samples, sample_rate, 0.90)
-functions.samples_to_seconds(len(final_samples),sample_rate)
+
+# samples, sample_rate = librosa.load(speaker_files[speaker_ids[0]][0], mono=True, sr=8000)
+# functions.samples_to_seconds(len(samples),sample_rate)
+# new_samples = feats.vad(samples, 0.01)
+# functions.samples_to_seconds(len(new_samples),sample_rate)
+# final_samples = functions.trim_signal(new_samples, sample_rate, 0.90)
+# functions.samples_to_seconds(len(final_samples),sample_rate)
 

b91bee1c7fa3c3a0604795378d12c35acbda70a4
prodriguez <rodriguez_p@fev.com>
2022-11-04 08:43:38 -0600

    commit message

--- read_files.py
+++ read_files.py
@@ -1,7 +1,7 @@
 import os
 
 print(os.getcwd())
-database_path = "/media/Data/databases"
+database_path = "/media/Data/databases/LibriSpeech"
 #path = "/Users/humanrobot/Documents/data/simpsons"
 
 dir = os.listdir(database_path)

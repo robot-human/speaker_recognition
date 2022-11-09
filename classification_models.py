dba0ffcc947c9570713891f5ad6a76e09e147a4f
prodriguez <rodriguez_p@fev.com>
2022-11-09 17:53:31 -0600

    commit message

--- classification_models.py
+++ classification_models.py
@@ -145,7 +145,7 @@ def run_GMM_model(speaker_ids, features, scaler):
             good_classifications += 1
         else:
             bad_classifications += 1
-        print(speaker)
+    print(good_classifications,bad_classifications)
     print("")
     return classifications
 
@@ -178,6 +178,6 @@ def run_SVM_model(speaker_ids, features, scaled_train, classes, scaler):
             good_classifications += 1
         else:
             bad_classifications += 1
-        print(speaker)
+    print(good_classifications,bad_classifications)
     print("")
     return classifications

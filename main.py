7757dddbdb2b6b1cff2b93ccce18bb1b79cddbfd
prodriguez <rodriguez_p@fev.com>
2022-11-09 08:48:00 -0600

    commit message

--- main.py
+++ main.py
@@ -34,10 +34,12 @@ ids, speaker_files = get_speaker_files(database_path)
 speaker_ids = random.sample(ids, k=N_SPEAKERS)
 signal_dict = get_speaker_signals_dict(speaker_files, speaker_ids)
 
-print(len(signal_dict[speaker_ids[0]]['train']))
-# window_frames = feats.get_window_frames_dict(speaker_ids, signal_dict , frames_attr)
+
+window_frames = feats.get_window_frames_dict(speaker_ids, signal_dict , frames_attr)
 # pow_frames = feats.get_pow_frames_dict(speaker_ids, window_frames, 512)
 # mfcc = feats.get_mfcc_feats(speaker_ids, pow_frames, mfcc_attr)
+
+print(window_frames[speaker_ids[0]]['train'])
 
 # classes = []
 # train_set = []

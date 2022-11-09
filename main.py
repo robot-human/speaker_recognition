1d12a7cec4ae72ec78fcebfe9403779c330e5a31
prodriguez <rodriguez_p@fev.com>
2022-11-09 11:40:16 -0600

    commit message

--- main.py
+++ main.py
@@ -96,6 +96,7 @@ print("MFFC with GMM")
 N_COMPONENTS = 200
 scaled_separate_set = []
 for id in speaker_ids:
+    print(features[id]['train'])
     scaled_separate_set.append(scaler.transform(features[id]['train']))
 
 speaker_gm_models = []

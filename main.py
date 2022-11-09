e6159d85f07fc9372faba0d5d46d89c376ad8fc9
prodriguez <rodriguez_p@fev.com>
2022-11-09 11:49:46 -0600

    commit message

--- main.py
+++ main.py
@@ -62,34 +62,34 @@ model_svm = svm.SVC(kernel='rbf')
 model_svm.fit(scaled_train,classes)
 
 
-# print("MFFC with VQ")
-# n_codewords = 50
-# epochs = 50
-# speaker_models = []
-# for enum, id in enumerate(speaker_ids):
-#     codebook, classes = models.vector_quantization_trainning(features[id]['train'], n_codewords, epochs)
-#     speaker_models.append(codebook)
+print("MFFC with VQ")
+n_codewords = 50
+epochs = 50
+speaker_models = []
+for enum, id in enumerate(speaker_ids):
+    codebook, classes = models.vector_quantization_trainning(features[id]['train'], n_codewords, epochs)
+    speaker_models.append(codebook)
     
-# for id in speaker_ids:
-#     speaker = -1
-#     dist = 1/0.00000001
-#     for enum, speaker_model in enumerate(speaker_models):
-#         classes = models.assign_classes(features[id]['test'], speaker_model)
-#         speaker_dist = models.featureset_distortion(features[id]['test'], classes, speaker_model)
-#         if(speaker_dist < dist):
-#             dist = speaker_dist
-#             speaker = enum
-#     print(speaker)
-# print("")
+for id in speaker_ids:
+    speaker = -1
+    dist = 1/0.00000001
+    for enum, speaker_model in enumerate(speaker_models):
+        classes = models.assign_classes(features[id]['test'], speaker_model)
+        speaker_dist = models.featureset_distortion(features[id]['test'], classes, speaker_model)
+        if(speaker_dist < dist):
+            dist = speaker_dist
+            speaker = enum
+    print(speaker)
+print("")
 
-# print("MFFC with SVM")
-# for enum, id in enumerate(speaker_ids):
-#     test_data = scaler.transform(features[id]['test'])
-#     test_classes = model_svm.predict(test_data)
-#     counts = np.bincount(test_classes)
-#     speaker = np.argmax(counts)
-#     print(speaker)
-# print("")
+print("MFFC with SVM")
+for enum, id in enumerate(speaker_ids):
+    test_data = scaler.transform(features[id]['test'])
+    test_classes = model_svm.predict(test_data)
+    counts = np.bincount(test_classes)
+    speaker = np.argmax(counts)
+    print(speaker)
+print("")
 
 
 print("MFFC with GMM")
@@ -117,54 +117,145 @@ print("")
 
 # ########################################################################################################
 # ## LPC
-# print("LPC with SVM")
-# features = feats.get_lpc_feats(speaker_ids, window_frames, 12)
-# classes = []
-# train_set = []
-# for enum, id in enumerate(speaker_ids):
-#     for val in features[id]['train']:
-#         train_set.extend(features[id]['train'])
-#         for i in range(len(features[id]['train'])):
-#             classes.append(enum)
+print("LPC")
+features = feats.get_lpc_feats(speaker_ids, window_frames, 12)
+classes = []
+train_set = []
+for enum, id in enumerate(speaker_ids):
+    for val in features[id]['train']:
+        train_set.extend(features[id]['train'])
+        for i in range(len(features[id]['train'])):
+            classes.append(enum)
 
 
 
-# scaler = StandardScaler()
-# scaler.fit(train_set)
-# scaled_train = scaler.transform(train_set)
-# model = svm.SVC(kernel='rbf')
-# model.fit(scaled_train,classes)
+scaler = StandardScaler()
+scaler.fit(train_set)
+scaled_train = scaler.transform(train_set)
+model_svm = svm.SVC(kernel='rbf')
+model_svm.fit(scaled_train,classes)
 
-# for enum, id in enumerate(speaker_ids):
-#     test_data = scaler.transform(features[id]['test'])
-#     test_classes = model.predict(test_data)
-#     counts = np.bincount(test_classes)
-#     speaker = np.argmax(counts)
-#     print(speaker)
-# print("")
+print("LPC with VQ")
+n_codewords = 50
+epochs = 50
+speaker_models = []
+for enum, id in enumerate(speaker_ids):
+    codebook, classes = models.vector_quantization_trainning(features[id]['train'], n_codewords, epochs)
+    speaker_models.append(codebook)
+    
+for id in speaker_ids:
+    speaker = -1
+    dist = 1/0.00000001
+    for enum, speaker_model in enumerate(speaker_models):
+        classes = models.assign_classes(features[id]['test'], speaker_model)
+        speaker_dist = models.featureset_distortion(features[id]['test'], classes, speaker_model)
+        if(speaker_dist < dist):
+            dist = speaker_dist
+            speaker = enum
+    print(speaker)
+print("")
+
+print("LPC with SVM")
+for enum, id in enumerate(speaker_ids):
+    test_data = scaler.transform(features[id]['test'])
+    test_classes = model_svm.predict(test_data)
+    counts = np.bincount(test_classes)
+    speaker = np.argmax(counts)
+    print(speaker)
+print("")
+
+
+print("LPC with GMM")
+N_COMPONENTS = 50
+scaled_separate_set = []
+for id in speaker_ids:
+    scaled_separate_set.append(scaler.transform(features[id]['train']))
+
+speaker_gm_models = []
+for sp in scaled_separate_set:
+    gm = GaussianMixture(n_components=N_COMPONENTS, random_state=0).fit(sp)
+    speaker_gm_models.append(gm)
+
+for id in speaker_ids:
+    dist = -1/0.000000001
+    speaker = -1
+    test_data = scaler.transform(features[id]['test'])
+    for enum, model in enumerate(speaker_gm_models):
+        speaker_dist = model.score(test_data)
+        if(speaker_dist > dist):
+            dist = speaker_dist
+            speaker = enum
+    print(speaker)
+print("")
 # ########################################################################################################
 # ## PLP
-# print("PLP with SVM")
-# features = feats.get_plp_feats(speaker_ids, pow_frames, 12, plp_filters)
-# classes = []
-# train_set = []
-# for enum, id in enumerate(speaker_ids):
-#     for val in features[id]['train']:
-#         train_set.extend(features[id]['train'])
-#         for i in range(len(features[id]['train'])):
-#             classes.append(enum)
+print("PLP")
+features = feats.get_plp_feats(speaker_ids, pow_frames, 12, plp_filters)
+classes = []
+train_set = []
+for enum, id in enumerate(speaker_ids):
+    for val in features[id]['train']:
+        train_set.extend(features[id]['train'])
+        for i in range(len(features[id]['train'])):
+            classes.append(enum)
 
 
 
-# scaler = StandardScaler()
-# scaler.fit(train_set)
-# scaled_train = scaler.transform(train_set)
-# model = svm.SVC(kernel='rbf')
-# model.fit(scaled_train,classes)
+scaler = StandardScaler()
+scaler.fit(train_set)
+scaled_train = scaler.transform(train_set)
+model_svm = svm.SVC(kernel='rbf')
+model_svm.fit(scaled_train,classes)
 
-# for enum, id in enumerate(speaker_ids):
-#     test_data = scaler.transform(features[id]['test'])
-#     test_classes = model.predict(test_data)
-#     counts = np.bincount(test_classes)
-#     speaker = np.argmax(counts)
-#     print(speaker)
+print("PLP with VQ")
+n_codewords = 50
+epochs = 50
+speaker_models = []
+for enum, id in enumerate(speaker_ids):
+    codebook, classes = models.vector_quantization_trainning(features[id]['train'], n_codewords, epochs)
+    speaker_models.append(codebook)
+    
+for id in speaker_ids:
+    speaker = -1
+    dist = 1/0.00000001
+    for enum, speaker_model in enumerate(speaker_models):
+        classes = models.assign_classes(features[id]['test'], speaker_model)
+        speaker_dist = models.featureset_distortion(features[id]['test'], classes, speaker_model)
+        if(speaker_dist < dist):
+            dist = speaker_dist
+            speaker = enum
+    print(speaker)
+print("")
+
+print("PLP with SVM")
+for enum, id in enumerate(speaker_ids):
+    test_data = scaler.transform(features[id]['test'])
+    test_classes = model_svm.predict(test_data)
+    counts = np.bincount(test_classes)
+    speaker = np.argmax(counts)
+    print(speaker)
+print("")
+
+
+print("PLP with GMM")
+N_COMPONENTS = 50
+scaled_separate_set = []
+for id in speaker_ids:
+    scaled_separate_set.append(scaler.transform(features[id]['train']))
+
+speaker_gm_models = []
+for sp in scaled_separate_set:
+    gm = GaussianMixture(n_components=N_COMPONENTS, random_state=0).fit(sp)
+    speaker_gm_models.append(gm)
+
+for id in speaker_ids:
+    dist = -1/0.000000001
+    speaker = -1
+    test_data = scaler.transform(features[id]['test'])
+    for enum, model in enumerate(speaker_gm_models):
+        speaker_dist = model.score(test_data)
+        if(speaker_dist > dist):
+            dist = speaker_dist
+            speaker = enum
+    print(speaker)
+print("")

4a7b811e89ed0bbbb0d7294aeb2adbd07da19052
prodriguez <rodriguez_p@fev.com>
2022-11-09 09:45:35 -0600

    commit message

--- main.py
+++ main.py
@@ -52,16 +52,16 @@ for enum, id in enumerate(speaker_ids):
         classes.append(enum)
 
 #print(train_set[0])
-#print(len(train_set),len(classes))
-scaler = StandardScaler()
-scaler.fit(train_set)
-scaled_train = scaler.transform(train_set)
-model = svm.SVC(kernel='rbf')
-model.fit(scaled_train,classes)
+print(len(train_set),len(classes))
+# scaler = StandardScaler()
+# scaler.fit(train_set)
+# scaled_train = scaler.transform(train_set)
+# model = svm.SVC(kernel='rbf')
+# model.fit(scaled_train,classes)
 
-for enum, id in enumerate(speaker_ids):
-    test_data = scaler.transform(mfcc[id]['test'])
-    test_classes = model.predict(test_data)
-    counts = np.bincount(test_classes)
-    speaker = np.argmax(counts)
-    print(speaker)
+# for enum, id in enumerate(speaker_ids):
+#     test_data = scaler.transform(mfcc[id]['test'])
+#     test_classes = model.predict(test_data)
+#     counts = np.bincount(test_classes)
+#     speaker = np.argmax(counts)
+#     print(speaker)

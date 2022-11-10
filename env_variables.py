b64bf46fab5da6c4390f25b24c6b1a31d301d188
prodriguez <rodriguez_p@fev.com>
2022-11-10 13:13:19 -0600

    commit message

--- env_variables.py
+++ env_variables.py
@@ -2,7 +2,7 @@ LOG_FILES_PATH = "/media/Data/pedro_tesis/log_files"
 DATABASE_PATH = "/media/Data/databases/LibriSpeech/train-clean-100/train-clean-100"
 
 N_SPEAKERS = 4
-SIGNAL_DURATION_IN_SECONDS = 1.0
+SIGNAL_DURATION_IN_SECONDS = 10.0
 
 SAMPLE_RATE = 10000
 NFFT = 512

38aab2ac5466b57909b18c428a28e718302180a4
prodriguez <rodriguez_p@fev.com>
2022-11-15 17:08:52 -0600

    commit message

--- env_variables.py
+++ env_variables.py
@@ -2,7 +2,7 @@ LOG_FILES_PATH = "/media/Data/pedro_tesis/log_files"
 DATABASE_PATH = "/media/Data/databases/LibriSpeech/train-clean-100/train-clean-100"
 
 N_SPEAKERS = 5
-SIGNAL_DURATION_IN_SECONDS = 5.0
+SIGNAL_DURATION_IN_SECONDS = 10.0
 
 SAMPLE_RATE = 10000
 NFFT = 512
@@ -45,6 +45,6 @@ MFCC_ATTR = {
 
 P = 12
 
-N_MIXTURES = 200
+N_MIXTURES = 500
 N_CODEWORDS = 400
 EPOCHS = 50

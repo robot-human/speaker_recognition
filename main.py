40495b888ca25c3c9169256c3b4db579509336fe
prodriguez <rodriguez_p@fev.com>
2022-11-18 03:28:24 -0600

    commit message

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ random.seed(10)
 date = datetime.datetime.now().strftime("%m/%d/%H/%M/%S").replace("/","_")
 
 
-MODELS_LIST = ['SVM']
+MODELS_LIST = ['VQ','GMM','SVM']
 FEATURES_LIST = ['MFCC','LPC','PLP']
 
 results = {

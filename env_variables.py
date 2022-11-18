e786899e361ee8ecaf2bd515388cdd260e615d64
prodriguez <rodriguez_p@fev.com>
2022-11-17 19:35:50 -0600

    commit message

--- env_variables.py
+++ env_variables.py
@@ -55,5 +55,5 @@ PLP_ATTR = {
 MODEL_ATTR = {
     "VQ" : {"N_CODEWORDS" : 40, "EPOCHS" : 50},
     "GMM" : {"N_MIXTURES" : min(350,math.floor(0.75*N_VECTOR_SAMPLES)), "EPOCHS" : 50},
-    "SVM" : {"EPOCHS" : 50}
+    "SVM" : {"EPOCHS" : 5000}
 }

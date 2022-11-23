7bc915aa9f04f0e34cc11f287fe240696363b85a
prodriguez <rodriguez_p@fev.com>
2022-11-22 23:08:02 -0600

    commit message

--- env_variables.py
+++ env_variables.py
@@ -2,4 +2,4 @@ LOG_FILE_PATH = "/media/Data/pedro_tesis/speaker_recognition/log_files/"
 DATABASE_PATH = "/media/Data/databases/LibriSpeech/train-clean-100/train-clean-100"
 MODELS_LIST = ['VQ','GMM','SVM']
 FEATURES_LIST = ['MFB','MFCC','LPC','PLP']
-PRINT_FILES = False
+PRINT_FILES = True

67d23c00fc20da01cc8c212a5a4f96da0925e4d5
prodriguez <rodriguez_p@fev.com>
2022-11-29 14:12:41 -0600

    commit message

--- send_audio_files.py
+++ send_audio_files.py
@@ -43,5 +43,47 @@ for idx in [7,11,25,40]:
             if(enum < 1):
                 audio_files_list.append(audio_path+"/"+audio)
 
-for audio in audio_files_list:
-    print(audio)
+def on_connect(client, userdata, flags, rc, properties=None):
+    print("on connect %s." % rc)
+def on_disconnect(client, userdata, flags, rc):
+    print("client disconnected")
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+def on_publish(client, userdata, mid, properties=None):
+    print("mid: " + str(mid))
+
+
+if __name__ == '__main__':
+    print("start sending files")
+    client = paho.Client(client_id=clientID, userdata=None, protocol=paho.MQTTv5)
+    client.on_connect = on_connect
+    client.connect(host, port, keepalive=KEEPALIVE)
+    client.on_subscribe = on_subscribe
+    client.subscribe(topic, qos=QOS)
+    client.on_publish = on_publish
+    
+    n_files = 0
+    for file_path in audio_files_list:
+        n_files += 1
+    
+    print(f"Number of files {n_files}")
+    #client.publish(topic, payload=f"Hello GUI am sending {n_files} files", qos=QOS)
+    publish.single(topic, payload=f"Hello GUI am sending {n_files} files", qos=QOS, retain=False, hostname=host,port=port, client_id=clientID, keepalive=KEEPALIVE, will=None, auth=None, tls=None,protocol=paho.MQTTv5, transport="tcp")
+    for name in audio_files_list:
+        print(name)
+        file_name_path = name
+        #client.publish(topic, payload=f"{name}", qos=QOS)
+        publish.single(topic, payload=f"{name}", qos=QOS, retain=False, hostname=host,port=port, client_id=clientID, keepalive=KEEPALIVE, will=None, auth=None, tls=None,protocol=paho.MQTTv5, transport="tcp")
+        sleep(0.2)
+        f = open(file_name_path, "r")
+        content = f.read()
+        print(getsizeof(content)/1000, " kbts")
+        #client.publish(topic, payload=content, qos=QOS)
+        publish.single(topic, payload=content, qos=QOS, retain=False, hostname=host,port=port, client_id=clientID, keepalive=KEEPALIVE, will=None, auth=None, tls=None,protocol=paho.MQTTv5, transport="tcp")
+        sleep(0.2)
+        f.close()
+        print(f"{name} closed")     
+    
+    client.on_disconnect = on_disconnect
+    client.disconnect()
+    
